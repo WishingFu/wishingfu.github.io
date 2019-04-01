f87a601e9cab2506af99d9c27b0600d786b47b71
fuwenxing <fuwenxing429@126.com>
2019-04-01 11:28:09 +0800

    数据库创建

---
+++ js/db.js
@@ -0,0 +1,61 @@
+class Database {
+
+    INITIALIZED = false;
+    db;
+
+    callback;
+
+    constructor(callback) {
+        this.callback = callback;
+        this.initDB();
+    }
+
+    initDB() {
+        const request = window.indexedDB.open("accounting", 1);
+        request.onsuccess = (e) => this.onInitSuccess(e);
+        request.onupgradeneeded = (e)=> this.onupgradeneeded(e);
+    }
+
+    onInitSuccess(event) {
+        this.INITIALIZED = true;
+        this.db = event.target.result;
+
+        this.callback && this.callback();
+    }
+
+    onUpgradeNeeded(event) {
+        const db = event.target.result;
+
+        const os = db.createObjectStore("record", {
+            keyPath: "id",
+            autoIncrement: true
+        });
+
+        os.createIndex("id", "id", {unique: true});
+        os.createIndex("tag", "tag");
+        os.createIndex("num", "num"); 
+        os.createIndex("desc", "desc");
+        os.createIndex("useTime", "useTime");
+        os.createIndex("createTime", "createTime");
+    }
+
+    t() {
+        return this.INITIALIZED && this.db.transaction("record", "readwrite");
+    }
+
+    r() {
+        return this.t().objectStore("record");
+    }
+}
+
+const DB = new Database(_ => {
+    const record = {};
+    record.tag = "早餐";
+    record.num = 150;
+    record.desc = "包子";
+    record.useTime = new Date();
+    record.createTime = new Date();
+
+    DB.r().add(record);
+});
+
