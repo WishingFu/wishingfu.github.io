804432e0e05da74a0a9b42681fb53422b351f4ae
fuwenxing <fuwenxing429@126.com>
2019-04-02 11:26:16 +0800

    remove id

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ function showRecord(record) {
 
     let html = 
     `<tr>
-        <td>id</td>
         <td>tag</td>
         <td>num</td>
         <td>useTime</td>
@@ -75,16 +74,16 @@ Date.prototype.format = function(fmt) {
        "s+" : this.getSeconds(),                 //秒 
        "q+" : Math.floor((this.getMonth()+3)/3), //季度 
        "S"  : this.getMilliseconds()             //毫秒 
-   }; 
-   if(/(y+)/.test(fmt)) {
-           fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
-   }
+    }; 
+    if(/(y+)/.test(fmt)) {
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+    }
     for(var k in o) {
-       if(new RegExp("("+ k +")").test(fmt)){
+        if(new RegExp("("+ k +")").test(fmt)){
             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
         }
     }
-   return fmt; 
+    return fmt; 
 }    
 
 let task;
